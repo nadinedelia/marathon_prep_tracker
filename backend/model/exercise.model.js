113c9c3aa159a471bc2f3d6ec7ad390a30c52f87
nadinedelia <nadine.delia@yahoo.com>
2023-10-05 16:53:11 +0700

    quick rf

--- backend/model/exercise.model.js
+++ backend/model/exercise.model.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const exerciseSchema = new Schema({
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
-}, {
-  timestamps: true,
-});
+const exerciseSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+// Add validation or custom methods to the schema if needed
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 
 // Middleware setup
 app.use(cors());
