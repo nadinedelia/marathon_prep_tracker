b61da62ff6e68637da5c52a1ca3b49c1086c8852
nadinedelia <nadine.delia@yahoo.com>
2023-10-05 19:12:37 +0700

    can create and get users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,25 +2,37 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 
-// GET: Retrieve all users
+// Middleware to parse the request body
+router.use(express.json());
+
 router.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
 });
 
-// POST: Add a new user
+// Add a new user
 router.post('/add', async (req, res) => {
+  console.log(req.body);  // Log the request body
+
   try {
     const { username } = req.body;
+
+    // Handle if username is not provided
+    if (!username) {
+      return res.status(400).json({ error: 'Username is required' });
+    }
+
     const newUser = new User({ username });
     await newUser.save();
     res.json({ message: 'User added successfully' });
   } catch (error) {
-    res.status(422).json({ error: 'Invalid input' });
+    console.error(error);
+    res.status(422).json({ error: error.message });
   }
 });
 
