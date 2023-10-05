d8806a77bcbd4b480ef817b50a82859b46a37642
nadinedelia <nadine.delia@yahoo.com>
2023-10-05 16:53:53 +0700

    making it prettier

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    },
   },
-}, {
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
