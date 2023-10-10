511d84198cce39852814c3828ab41204324643c3
nadinedelia <nadine.delia@yahoo.com>
2023-10-10 18:53:39 +0700

    install scripts update

--- install.sh
+++
@@ -1,3 +0,0 @@
-echo "deb [ arch=amd64,arm6
-4 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt-get install -y mongodb-mongosh
