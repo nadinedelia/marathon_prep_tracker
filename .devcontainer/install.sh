d05fd27ced2055740c5f3bca156e975a909e3fb2
nadinedelia <nadine.delia@yahoo.com>
2023-10-10 19:39:04 +0700

    install script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,13 +1,3 @@
-sudo apt update
-sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
 wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add - \
     && echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list \
     && sudo apt-get update \
