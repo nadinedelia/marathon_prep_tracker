fb7b9d56afb3a36ebc88fef3329cf8a6d0590b0c
nadinedelia <nadine.delia@yahoo.com>
2023-10-10 17:30:34 +0700

    dockerfile load

---
+++ install.sh
@@ -0,0 +1,3 @@
+echo "deb [ arch=amd64,arm6
+4 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get install -y mongodb-mongosh
