119de64e94227564ad3e613af0273782431e9e7c
nadinedelia <nadine.delia@yahoo.com>
2023-11-10 16:40:36 +0800

    quick api change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 function getUrl() {
-    if (process.env.REACT_APP_API_BASE_URL) {
-        return process.env.REACT_APP_API_BASE_URL;
+    if (process.env.REACT_APP_API_GATEWAY_URL) {
+        return process.env.REACT_APP_API_GATEWAY_URL;
     } else {
-        // Fallback for local development outside Docker
-        return `http://localhost:5300`;
+        return `http://localhost/activity/`;
     }
 }
 
